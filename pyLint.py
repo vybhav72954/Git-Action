f59e61c88d22d419f639ceaa98c1eea3d6ed672a
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-10-27 16:36:38 +0530

    Update pyLint.py

--- pyLint.py
+++ pyLint.py
@@ -16,7 +16,7 @@ parser.add_argument('-p',
                     help='path to directory you want to run pylint | '
                          'Default: %(default)s | '
                          'Type: %(type)s ',
-                    default='./Python/add.py',
+                    default='/Python',
                     nargs='+',
                     type=str)
 
